8d5f94b161df292b7e7e459dd0357b8f0327abef
Rafał Baran <70489677+rafal06@users.noreply.github.com>
2023-08-04 10:59:34 +0200

    Bind winnerCount context value to settings form input

--- src/components/SettingsForm.tsx
+++ src/components/SettingsForm.tsx
@@ -1,11 +1,40 @@
 import {Button, FormControl, FormLabel, FormText, Row} from "react-bootstrap";
+import {useContext} from "preact/hooks";
+import {SettingsContext} from "./SettingsContext.tsx";
 
 export default function SettingsForm() {
+    const { settings, setSettings } = useContext(SettingsContext);
+
+    const handleWinnerCountChange = event => {
+        const newValue = Number(event.target.value);
+        if (newValue < 0) {
+            setSettings({...settings});
+            return;
+        }
+
+        setSettings({
+            ...settings,
+            winnerCount: newValue,
+        })
+    }
+
+    const handleWinnerCountBlur = event => {
+        if (event.target.value == 0) {
+            setSettings({
+                ...settings,
+                winnerCount: 1,
+            })
+        }
+    }
+
     return (
         <Row className="g-3">
             <div>
                 <FormLabel>Number of winners</FormLabel>
-                <FormControl type="number" min={1} />
+                <FormControl type="number" min={1}
+                             value={settings.winnerCount}
+                             onChange={handleWinnerCountChange}
+                             onBlur={handleWinnerCountBlur} />
             </div>
             <div>
                 <FormLabel>Must follow</FormLabel>
