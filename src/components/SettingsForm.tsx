069c9dff7ddf35674af51af30ef475ceab711458
Rafał Baran <70489677+rafal06@users.noreply.github.com>
2023-08-04 19:57:04 +0200

    Move mustFollow input to a separate component

--- src/components/SettingsForm.tsx
+++ src/components/SettingsForm.tsx
@@ -1,6 +1,7 @@
-import {Button, FormControl, FormLabel, FormText, Row} from "react-bootstrap";
+import {Button, FormControl, FormLabel, Row} from "react-bootstrap";
 import {useContext, useState} from "preact/hooks";
 import {SettingsContext} from "./SettingsContext.tsx";
+import MustFollowInput from "./settings-form/MustFollowInput.tsx";
 
 export default function SettingsForm() {
     const { settings, setSettings } = useContext(SettingsContext);
@@ -27,44 +28,6 @@ export default function SettingsForm() {
         }
     }
 
-    const [isCorrectUsernameFormat, setIsCorrectUsernameFormat] = useState(true);
-    const [isFirstMustFollowInput, setIsFirstMustFollowInput] = useState(true);
-
-    const handleMustFollowChange = event => {
-        const userArr: Array<string> = [];
-        event.target.value.split(',').forEach(value => {
-            value = value.trim();
-            if (value !== '') userArr.push(value);
-        });
-
-        setSettings({
-            ...settings,
-            mustFollow: userArr,
-        });
-
-        if (userArr.length === 0) {
-            setIsCorrectUsernameFormat(true);
-            return;
-        }
-
-        for (const username of userArr) {
-            if (/@[^\s@,]+?@[^\s@,]+?\.[^\s@,]+/i.test(username)) continue;
-
-            setIsCorrectUsernameFormat(false);
-            return;
-        }
-
-        setIsCorrectUsernameFormat(true);
-    }
-
-    const handleMustFollowBlur = event => {
-        setIsFirstMustFollowInput(false);
-        handleMustFollowChange(event);
-        if (settings.mustFollow.length === 0) {
-            setIsCorrectUsernameFormat(false);
-        }
-    }
-
     return (
         <Row className="g-3">
             <div>
@@ -75,12 +38,7 @@ export default function SettingsForm() {
                              onBlur={handleWinnerCountBlur} />
             </div>
             <div>
-                <FormLabel>Must follow</FormLabel>
-                <FormControl placeholder="@someone@example.social"
-                             isInvalid={!isCorrectUsernameFormat}
-                             onChange={isFirstMustFollowInput ? undefined : handleMustFollowChange}
-                             onBlur={handleMustFollowBlur}/>
-                <FormText>Separate usernames with a comma</FormText>
+                <MustFollowInput />
             </div>
             <div>
                 <Button disabled={true} variant="outline-primary" className="mt-2 w-100">Continue</Button> {/*A better name might be 'Select winner'*/}
---
+++ src/components/settings-form/MustFollowInput.tsx
@@ -0,0 +1,56 @@
+import {FormControl, FormLabel, FormText} from "react-bootstrap";
+import {useContext, useState} from "preact/hooks";
+import {SettingsContext} from "../SettingsContext.tsx";
+
+export default function MustFollowInput() {
+    const { settings, setSettings } = useContext(SettingsContext);
+
+    const [isCorrectFormat, setIsCorrectFormat] = useState(true);
+    const [isFirstInput, setIsFirstInput] = useState(true);
+
+    const handleChange = event => {
+        const userArr: Array<string> = [];
+        event.target.value.split(',').forEach(value => {
+            value = value.trim();
+            if (value !== '') userArr.push(value);
+        });
+
+        setSettings({
+            ...settings,
+            mustFollow: userArr,
+        });
+
+        if (userArr.length === 0) {
+            setIsCorrectFormat(true);
+            return;
+        }
+
+        for (const username of userArr) {
+            if (/@[^\s@,]+?@[^\s@,]+?\.[^\s@,]+/i.test(username)) continue;
+
+            setIsCorrectFormat(false);
+            return;
+        }
+
+        setIsCorrectFormat(true);
+    }
+
+    const handleBlur = event => {
+        setIsFirstInput(false);
+        handleChange(event);
+        if (settings.mustFollow.length === 0) {
+            setIsCorrectFormat(false);
+        }
+    }
+
+    return (
+        <>
+            <FormLabel>Must follow</FormLabel>
+            <FormControl placeholder="@someone@example.social"
+                         isInvalid={!isCorrectFormat}
+                         onChange={isFirstInput ? undefined : handleChange}
+                         onBlur={handleBlur}/>
+            <FormText>Separate usernames with a comma</FormText>
+        </>
+    )
+}
