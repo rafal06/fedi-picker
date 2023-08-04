ae3634ccaea22fdf0804adb6400bd1705599a894
Rafał Baran <70489677+rafal06@users.noreply.github.com>
2023-08-04 10:56:05 +0200

    Create settings context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import Form from "./components/Form.tsx";
 import Navbar from "./components/Navbar.tsx";
+import SettingsProvider from "./components/SettingsContext.tsx";
 
 export default function App() {
 
-  return (
-    <>
-      <Navbar />
-      <Form />
-    </>
-  )
+    return (
+        <>
+            <Navbar />
+            <SettingsProvider>
+                <Form />
+            </SettingsProvider>
+        </>
+    )
 }
