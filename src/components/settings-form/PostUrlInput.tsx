2ab93d161ea49743cc6e6ba6b68bf6fed9f74fb5
Rafał Baran <70489677+rafal06@users.noreply.github.com>
2023-08-06 11:11:03 +0200

    Bind postUrl input to settings context

--- src/components/settings-form/PostUrlInput.tsx
+++ src/components/settings-form/PostUrlInput.tsx
@@ -1,10 +1,23 @@
 import {FormControl, FormLabel} from "react-bootstrap";
+import {useContext} from "preact/hooks";
+import {SettingsContext} from "../SettingsContext.tsx";
 
 export default function PostUrlInput() {
+    const { settings, setSettings } = useContext(SettingsContext);
+
+    const handleChange = event => {
+        setSettings({
+            ...settings,
+            postUrl: event.target.value,
+        })
+    }
+
     return (
         <>
             <FormLabel>Post URL</FormLabel>
-            <FormControl placeholder="https://example.social/@someone/110815462944644507" />
+            <FormControl placeholder="https://example.social/@someone/110815462944644507"
+                         value={settings.postUrl}
+                         onInput={handleChange} />
         </>
     )
 }
